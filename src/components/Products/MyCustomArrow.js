f8a5f015bd94529f7e7756091097659ed2d6cce6
Gio <admin@Gios-MBP.Ueevii>
2023-01-20 14:26:45 -0500

    arrows

--- src/components/Products/MyCustomArrow.js
+++ src/components/Products/MyCustomArrow.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Card.css'
+import { MdArrowBackIosNew } from 'react-icons/md'
+import { MdArrowForwardIos } from 'react-icons/md'
 
 function MyCustomArrow({ id, onClick }) {
 
   return (
     <div className='customArrow' >
-      {id === 'prev' ? <button className='prevArrow' onClick={onClick}> Prev </button> : <button className='nextArrow' onClick={onClick}> Next</button>}
+      {id === 'prev' ? <button className='prevArrow' onClick={onClick}> <MdArrowBackIosNew/> </button> : <button className='nextArrow' onClick={onClick}> <MdArrowForwardIos/></button>}
     </div>
   )
 }
